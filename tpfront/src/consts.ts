c93445a121837bcfafce90bcf7f352e1a3dc37e4
FacundoBenavente <111788429+FacundoBenavente@users.noreply.github.com>
2026-04-10 13:40:19 -0300

    Pequeño cambio (ahora si ya no hay más)

--- tpfront/src/consts.ts
+++ tpfront/src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Oraculo de Dados';
+export const SITE_DESCRIPTION = 'Tirá los dados y dejá que el destino decida por vos';
